0432fd63bb3cdb4bf30ab6f93f87dc1c53f52003
Tomasz Mikus <mikus.tomasz@gmail.com>
2024-06-06 21:51:10 +0100

    Add the initial project files.

---
+++ start.sh
@@ -0,0 +1,9 @@
+#!/usr/bin/env bash
+
+SCRIPT_DIR=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
+[[ -d "${SCRIPT_DIR}/.venv" ]] || python -m venv "${SCRIPT_DIR}/.venv" # Make sure venv exists
+. "${SCRIPT_DIR}/.venv/bin/activate"
+
+pip install -r requirements.txt
+
+jupyter notebook
